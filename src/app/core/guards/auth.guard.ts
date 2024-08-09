34f4d82a8a003dd6a5690a1b508619fcc570c814
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-08-09 13:36:47 -0400

    TEST ERROR

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -25,9 +25,15 @@ export class AuthGuard implements CanActivate {
     | UrlTree {
     const token = localStorage.getItem('auth_token');
     if (token) {
-      const tokenPayload = this.jwtHelper.decodeToken(token);
-      console.log("***tokenPayload***");
-      console.log(tokenPayload);
+      try {
+        const tokenPayload = this.jwtHelper.decodeToken(token);
+        console.log("***tokenPayload***");
+        console.log(tokenPayload);
+      } catch (error) {
+        console.log("***ERROR***");
+        console.log(error);
+        
+      }
       return true;
     }
     this.router.navigate(['/'], {
