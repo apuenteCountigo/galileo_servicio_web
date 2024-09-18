a7e547e5b47a3e738d4afdbae4895d9abb4d01e4
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-18 13:31:14 -0400

    2-new Structure PageableObjectResponse

--- src/app/core/components/csv/csv-list.component.ts
+++ src/app/core/components/csv/csv-list.component.ts
@@ -37,8 +37,8 @@ export class CsvListComponent implements OnInit {
     console.log(this.filters);
     console.log(this.objetivos);
     console.log(this.operacion);
-    
-    this.loadCSV();
+    this.loadNodesForPage(1);
+    // this.loadCSV();
   }
 
   loadCSV(){
@@ -131,6 +131,8 @@ export class CsvListComponent implements OnInit {
         });
       }
     }
+    console.log(this.nodes);
+    
   }
 
   openFolder(node: FileNode): void {
