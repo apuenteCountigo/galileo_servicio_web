7a19d27520e29d3c0f59b367c91302904948059e
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-25 17:47:27 -0400

    FIX mapAddress in table servidores

--- src/app/core/models/server.model.ts
+++ src/app/core/models/server.model.ts
@@ -5,6 +5,7 @@ export class Server extends Resource {
   public id!: number;
   public servicio!: string;
   public ipServicio?: string;
+  public mapAddress?: string;
   public puerto?: string;
   public usuario?: string;
   public password?: string;
